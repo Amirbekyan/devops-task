17787152af2cfce4dad1aa8b939a96455d161f54
Moushegh Amirbekyan <mousheghamirbekyan@gmail.com>
2025-11-06 17:50:57 +0400

    * add argo-rollouts * add a rollout to olleh chart

---
+++ argo-rollouts.tf
@@ -0,0 +1,42 @@
+locals {
+  argo_rollouts = {
+    namespace_name        = "argo-rollouts"
+    values_tpl            = "${path.module}/src/helm/argo-rollouts-values-tpl.yml"
+    dashboard_enabled     = true
+    ingress_hostname      = "argorol.devops-task"
+    env                   = "devops-task"
+    notifications_enabled = false
+    slack_channel         = "var.argocd_notifications.channel"
+    slack_oauth_token     = "var.argocd_notifications.oauth_token"
+    prometheus_enabled    = false
+  }
+}
+
+resource "kubernetes_namespace" "argo_rollouts" {
+  metadata {
+    name = local.argo_rollouts.namespace_name
+  }
+}
+
+resource "helm_release" "argo_rollouts" {
+  name       = "argo-rollouts"
+  namespace  = kubernetes_namespace.argo_rollouts.id
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-rollouts"
+  version    = "2.40.5"
+
+  values = [
+    templatefile(local.argo_rollouts.values_tpl, {
+      dashboard_enabled     = local.argo_rollouts.dashboard_enabled
+      ingress_hostname      = local.argo_rollouts.ingress_hostname
+      env                   = local.argo_rollouts.env
+      notifications_enabled = local.argo_rollouts.notifications_enabled
+      slack_channel         = local.argo_rollouts.slack_channel
+      slack_oauth_token     = local.argo_rollouts.slack_oauth_token
+      prometheus_enabled    = local.argo_rollouts.prometheus_enabled
+      prometheus_labels = indent(8, yamlencode({
+        release = "prometheus"
+      }))
+    })
+  ]
+}
